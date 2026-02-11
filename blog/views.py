a9920fa16998397e9a80af557fd9e98ab009fc95
akcodespy <akmoolath@gmail.com>
2026-02-11 22:24:47 +0530

    added templates,style and urls.stable

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import Post, Comment
+
+
+def blog_category(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by("-created_on")
+    context = {
+        "category": category,
+        "posts": posts,
+    }
+    return render(request, "home.html", context)
+
+
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+
+    return render(request, "blog_detail.html", context)
--- blog_main/views.py
+++ blog_main/views.py
@@ -1,2 +1,9 @@
 from django.shortcuts import render
+from blog.models import Post
 
+def home(request):
+    posts = Post.objects.all().order_by("-created_on")
+    context = {
+        "posts": posts,
+    }
+    return render(request, "home.html", context)
