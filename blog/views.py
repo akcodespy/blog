80098da8ccab7ef847da6b4c0e31389dc85478da
akcodespy <akmoolath@gmail.com>
2026-02-11 22:45:20 +0530

    added comments

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
 from blog.models import Post, Comment
+from blog.forms import CommentForm
+
 
 
 def blog_category(request, category):
@@ -17,10 +19,22 @@ def blog_category(request, category):
 
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
+            )
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
+
     comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
         "comments": comments,
+        "form": form,
     }
-
     return render(request, "blog_detail.html", context)
